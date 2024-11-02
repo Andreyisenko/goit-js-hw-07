cd5ed67bab5c248e454b885223cd4f1020dd8bcd
Andrey Isenko <aisenko87@gmail.com>
2024-11-02 18:08:02 +0200

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,15 @@
-const totList = document.querySelector("#categories")
-console.log(totList);
-totList.querySelectorAll(".item")
-console.log(totList.querySelectorAll(".item").length);
+const totList = document.querySelector('#categories');
+console.log(`Number of categories: ${totList.children.length}`);
+const list = totList.querySelectorAll('.item');
+console.log(`Category: ${list[0].children[0].textContent}`);
+console.log(
+  `Elements: ${list[0].children[0].nextElementSibling.children.length}`
+);
+console.log(`Category: ${list[1].children[0].textContent}`);
+console.log(
+  `Elements: ${list[1].children[0].nextElementSibling.children.length}`
+);
+console.log(`Category: ${list[2].children[0].textContent}`);
+console.log(
+  `Elements: ${list[2].children[0].nextElementSibling.children.length}`
+);
