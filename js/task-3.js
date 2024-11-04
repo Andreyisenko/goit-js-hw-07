9a5c41df57a34802893124b9e461015e6a7d42bd
Andrey Isenko <aisenko87@gmail.com>
2024-11-04 20:12:14 +0200

    task-4

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@ const inpItem = document.querySelector('#name-input');
 const spnItem = document.querySelector('#name-output');
 // console.log(spnItem.textContent);
 
-inpItem.addEventListener('input', event => {
+inpItem.addEventListener('submit', event => {
   spnItem.textContent = inpItem.value.trim();
   if (event.target.value.trim() === '') {
     spnItem.textContent = 'Anonymous';
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,39 @@
+const formA1 = document.querySelector('.login-form');
+const btnLog = document.querySelector('button');
+const inpAll = document.querySelectorAll('input');
+// console.log(inpAll);
+formA1.style.display = 'flex';
+formA1.style.backgroundColor = '#fff';
+formA1.style.alignItems = 'flex-start';
+formA1.style.justifyContent = 'flex-start';
+formA1.style.flexDirection = 'column';
+formA1.style.gap = '16px';
+formA1.style.borderRadius = '8px';
+formA1.style.padding = '24px';
+formA1.style.width = '408px';
+formA1.style.height = '256px';
 
+btnLog.style.display = 'flex';
+btnLog.style.alignItems = 'flex-start';
+btnLog.style.justifyContent = 'flex-start';
+btnLog.style.flexDirection = 'row';
+btnLog.style.borderRadius = '8px';
+btnLog.style.gap = '10px';
+btnLog.style.padding = '8px 16px';
+btnLog.style.width = '86px';
+btnLog.style.height = '40px';
+btnLog.style.backgroundColor = '#4e75ff';
+btnLog.style.color = '#fff';
+btnLog.style.fontWeight = '500';
+btnLog.style.fontSize = '16px';
+
+formA1.addEventListener('submit', event => {
+  event.preventDefault();
+  const userEmail = event.target.elements.email.value;
+  const userPassword = event.target.elements.password.value;
+  if ('' === userPassword.trim() || userEmail.trim() === '') {
+    alert('All form fields must be filled in');
+  }
+  console.log(`{email: ${userEmail.trim()}, password: ${userPassword.trim()}}`);
+  formA1.reset();
+});
