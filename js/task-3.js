61a79fad8d244e3d3698b481184482d0a5a75d9d
Andrey Isenko <aisenko87@gmail.com>
2024-11-04 21:13:36 +0200

    task-5

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,12 @@
 const inpItem = document.querySelector('#name-input');
 const spnItem = document.querySelector('#name-output');
 // console.log(spnItem.textContent);
-
-inpItem.addEventListener('submit', event => {
+inpItem.style.width = "360px"
+inpItem.style.height = "40px"
+inpItem.addEventListener('input', event => {
   spnItem.textContent = inpItem.value.trim();
   if (event.target.value.trim() === '') {
     spnItem.textContent = 'Anonymous';
   }
+  return spnItem.textContent;
 });
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,28 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const bodY = document.querySelector('body');
+const spaN = document.querySelector('.color');
+const btn = document.querySelector('.change-color');
+btn.style.display = 'flex';
+btn.style.alignItems = 'center';
+btn.style.justifyContent = 'center';
+btn.style.flexDirection = 'row';
+btn.style.borderRadius = '8px';
+btn.style.gap = '10px';
+btn.style.padding = '8px 16px';
+btn.style.width = '148px';
+btn.style.height = '40px';
+btn.style.backgroundColor = '#4e75ff';
+btn.style.color = '#fff';
+btn.style.fontWeight = '500';
+btn.style.fontSize = '16px';
+
+btn.addEventListener('click', () => {
+  bodY.style.backgroundColor = `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+  spaN.textContent = `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+});
