4839e47c5468e1145858cb9b985d85004b5e09df
Andrey Isenko <aisenko87@gmail.com>
2024-11-05 09:02:01 +0200

    task-6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,47 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const divBox = document.querySelector('#boxes');
+const inputNumb = document.querySelector('input');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+
+let size = 30;
+
+btnCreate.addEventListener('click', createMarkup);
+
+function createMarkup() {
+  if (Number(inputNumb.value) <= 0 || Number(inputNumb.value) > 100) {
+    console.log('error');
+    return;
+  }
+  createBoxes(inputNumb.value);
+  Number;
+}
+function createBoxes(amount) {
+  divBox.innerHTML = '';
+  inputNumb.value = '';
+  size = 30;
+  const arr = [];
+  for (let index = 0; index < amount; index++) {
+    const myBox = document.createElement('div');
+    myBox.style.width = `${size}px`;
+    myBox.style.height = `${size}px`;
+    myBox.style.backgroundColor = getRandomHexColor();
+    arr.push(myBox);
+    size += 10;
+  }
+
+  divBox.append(...arr);
+
+  console.log(inputNumb.value);
+}
+
+btnDestroy.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  divBox.innerHTML = '';
+   
+
+}
